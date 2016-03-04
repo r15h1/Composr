0c29d945734e9e1c2a844799da6d8b67d514053e
r15h1 <tapsee.rishi@gmail.com>
2016-03-04 12:03:58 -0500

    add missing files

---
+++ src/Composr.Web/Models/PostRequestModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Composr.Web.Models
+{
+    public class PostRequestModel
+    {
+        public int BlogID { get; set; }
+
+        public string Search { get; set; }
+    }
+}
---
+++ src/Composr.Web/MultiTenancy/TenantResolver.cs
@@ -0,0 +1,41 @@
+﻿using Composr.Core;
+using Microsoft.AspNet.Http;
+using SaasKit.Multitenancy;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Composr.Web.MultiTenancy
+{
+    public class BlogResolver : ITenantResolver<Blog>
+    {
+        IEnumerable<Blog> blogs = new List<Blog>(new[]
+        {
+            new Core.Blog(1) {Description="Blog 1 description", Locale=Core.Locale.EN, Name="Blog1", Url="localhost:5000" },
+            new Core.Blog(2) {Description="Blog 2 description", Locale=Core.Locale.EN, Name="Blog2", Url="localhost:5001" },
+            new Core.Blog(3) {Description="Blog 3 description", Locale=Core.Locale.EN, Name="Blog3", Url="localhost:5002" },
+            new Core.Blog(4) {Description="Blog 4 description", Locale=Core.Locale.EN, Name="Blog4", Url="localhost:5003" }
+
+        });
+        public Task<TenantContext<Blog>> ResolveAsync(HttpContext context)
+        {
+            TenantContext<Blog> ctx = null;
+            Blog blog = null;
+
+            if (context.Request.Path.HasValue && context.Request.Path.Value.StartsWith("/admin/blogs"))
+            {
+                blog = blogs.FirstOrDefault(t => context.Request.Path.Value.StartsWith("/admin/blogs/" + t.Id.ToString()));
+            }
+            else {
+                blog = blogs.FirstOrDefault(t => t.Url.Equals(context.Request.Host.Value.ToLower()));
+            }
+
+            if (blog != null)
+            {
+                ctx = new TenantContext<Blog>(blog);
+            }
+
+            return Task.FromResult(ctx);
+        }
+    }
+}

0959ca22f07d0b63eaf358b21a4d8eeb76d1792b
r15h1 <tapsee.rishi@gmail.com>
2016-05-11 14:41:43 -0400

    clean directory before generating index

--- src/Composr.Indexing.Client/Program.cs
+++ src/Composr.Indexing.Client/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Composr.Util;
 using Microsoft.Extensions.Logging;
+using System;
 
 namespace Composr.Indexing.Client
 {
@@ -8,9 +9,25 @@ namespace Composr.Indexing.Client
         public static void Main(string[] args)
         {
             ILogger logger = Logging.CreateLogger<Program>();
-            logger.LogInformation("starting indexing program");
-            new LuceneClient(new LuceneIndexWriter()).GenerateIndex();
-            logger.LogInformation("indexing completed");
+            try {               
+                logger.LogInformation("starting indexing program");
+
+                logger.LogInformation($"deleting current index in {Configuration.IndexDirectory}");
+                ClearIndexDirectory();
+
+                new LuceneClient(new LuceneIndexWriter()).GenerateIndex();
+                logger.LogInformation("indexing completed");
+            }
+            catch(Exception ex)
+            {
+                logger.LogCritical($"error: {ex.Message + Environment.NewLine + ex.StackTrace}");
+            }
+        }
+
+        private static void ClearIndexDirectory()
+        {
+            foreach (var file in System.IO.Directory.EnumerateFiles(Configuration.IndexDirectory))
+                System.IO.File.Delete(file);            
         }
     }
 }

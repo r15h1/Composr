0ab99eaeee71a547ca4cda873d1cc8883d94c395
r15h1 <tapsee.rishi@gmail.com>
2016-05-17 09:56:34 -0400

    media queries for smoother jumbotron transition

---
+++ src/Composr.Core/SearchResult.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Composr.Core
+{
+    /// <summary>
+    /// data structure to represent a post in the form of a search result
+    /// </summary>
+    public class SearchResult
+    {
+        public string Title { get; set; }
+        public string Snippet { get; set; }
+        public string URN { get; set; }
+        public string Id { get; set; }
+        public string DatePublished { get; set; }
+        public string Body{ get; set; }
+    }
+
+    public enum SearchSortOrder
+    {
+        Title,
+        DatePublished,
+        BestMatch
+    }
+
+    public class SearchCriteria
+    {
+        public SearchCriteria()
+        {
+            SearchSortOrder = SearchSortOrder.BestMatch;
+        }
+
+        public int BlogID { get; set; }
+        public Locale Locale { get; set; }
+        public string SearchTerm { get; set; }
+        public SearchSortOrder SearchSortOrder { get; set; }
+        public string URN { get; set; }
+
+    }
+}
---
+++ src/Composr.Indexing/SearchEngine.cs
@@ -0,0 +1,96 @@
+﻿using Composr.Core;
+using Composr.Util;
+using Lucene.Net.Index;
+using Lucene.Net.Search;
+using Lucene.Net.Store;
+using System.Collections.Generic;
+using System;
+using Lucene.Net.Documents;
+using System.Linq;
+
+namespace Composr.Indexing
+{
+    public class SearchEngine
+    {
+        private static IndexReader reader;
+        private static Directory directory;
+
+        static SearchEngine()
+        {
+            directory = new RAMDirectory(FSDirectory.Open(Configuration.IndexDirectory));
+            reader = IndexReader.Open(directory, true);
+        }
+
+        public IList<SearchResult> Search(SearchCriteria criteria)
+        {
+            IndexSearcher searcher = new IndexSearcher(reader);
+            Query query = CreateQuery(criteria);
+            TopDocs docs = searcher.Search(query, 25);
+            CompileOptions options = string.IsNullOrWhiteSpace(criteria.URN) ? CompileOptions.Exclude_Post_Body : CompileOptions.Include_Post_Body;
+            return CompileResults(searcher, docs, options);
+        }
+
+        private IList<SearchResult> CompileResults(IndexSearcher searcher, TopDocs docs, CompileOptions options = CompileOptions.Exclude_Post_Body)
+        {
+            List<SearchResult> results = new List<SearchResult>();
+            foreach (var doc in docs.ScoreDocs)
+                results.Add(CompileResult(searcher.Doc(doc.Doc), options));
+
+            return results;
+        }
+
+        private SearchResult CompileResult(Document doc, CompileOptions options)
+        {
+            SearchResult result = new SearchResult();
+            result.DatePublished = doc.Get(IndexFields.PostDatePublished);
+            result.Id = doc.Get(IndexFields.PostID);            
+            result.Title = doc.Get(IndexFields.PostTitle);
+            result.URN = doc.Get(IndexFields.PostURN);
+
+            if(options == CompileOptions.Include_Post_Body)
+                result.Body = doc.Get(IndexFields.PostBody);
+            else
+                result.Snippet = doc.Get(IndexFields.PostSnippet);
+
+            return result;
+        }
+
+        private Query CreateQuery(SearchCriteria criteria)
+        {
+            BooleanQuery query = new BooleanQuery();
+            TermQuery q = null;
+
+            if (!string.IsNullOrWhiteSpace(criteria.SearchTerm))
+            {
+                q = new TermQuery(new Term(IndexFields.PostTitle, criteria.SearchTerm.ToLowerInvariant()));
+                q.Boost = 1.5f;
+                query.Add(q, Occur.SHOULD);
+
+                q = new TermQuery(new Term(IndexFields.PostBody, criteria.SearchTerm.ToLowerInvariant()));
+                q.Boost = 1.0f;
+                query.Add(q, Occur.SHOULD);
+            }
+
+            if (!string.IsNullOrWhiteSpace(criteria.URN))
+            {
+                q = new TermQuery(new Term(IndexFields.PostURN, criteria.URN.ToLowerInvariant()));
+                q.Boost = 1.5f;
+                query.Add(q, Occur.MUST);
+            }
+
+            q = new TermQuery(new Term(IndexFields.BlogID, criteria.BlogID.ToString()));
+            query.Add(q, Occur.MUST);
+
+            q = new TermQuery(new Term(IndexFields.Locale, criteria.Locale.ToString()));
+            query.Add(q, Occur.MUST);
+
+            return query;
+        }
+    }
+
+    public enum CompileOptions
+    {
+        Exclude_Post_Body,
+        Include_Post_Body
+    }
+}

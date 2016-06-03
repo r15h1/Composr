f7f942edfcf8067997352a9ed1d9aa297ffefb9d
r15h1 <tapsee.rishi@gmail.com>
2016-06-03 09:46:11 -0400

    moved mock repository under Composr.Lib.Repository.Mock

--- src/Composr.Lib/Repository/Mock/PostRepository.cs
+++ src/Composr.Lib/Repository/Mock/PostRepository.cs
@@ -4,7 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-namespace Composr.Mock.Repositories
+namespace Composr.Lib.Repositories.Mock
 {
     public class PostRepository : IRepository<Post>
     {

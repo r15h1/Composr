ee3bebeddc69737d502bc7544424e753af2436bd
r15h1 <tapsee.rishi@gmail.com>
2016-09-13 16:54:45 -0400

    #65 - added international hreflang targeting for multiple languages for home and details pages

---
+++ src/Composr.Web/Middleware/HrefLangLocalizerCollection.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Extensions.Localization;
+using Composr.Core;
+using System.Globalization;
+
+namespace Composr.Web.Middleware
+{
+    public class HrefLangLocalizerCollection
+    {
+        private IStringLocalizer localizer;
+        private Locale currentLocale;
+        public HrefLangLocalizerCollection(Locale currentLocale, IStringLocalizer localizer)
+        {
+            this.localizer = localizer;
+            this.currentLocale = currentLocale;
+        }
+
+        public IEnumerable<HrefLangLocalizer> GetAlternateLocalizers()
+        {
+            foreach(var locale in Enum.GetValues(typeof(Locale)))
+                if((Locale) locale != currentLocale)
+                    yield return new HrefLangLocalizer((Locale) locale, localizer.WithCulture(new CultureInfo(((Locale)locale).ToString())));
+        }
+    }
+
+    public class HrefLangLocalizer
+    {
+        public HrefLangLocalizer (Locale locale, IStringLocalizer localizer)
+        {
+            Localizer = localizer;
+            Locale = locale;
+        }
+
+        public IStringLocalizer Localizer { get; private set; }
+        public Locale Locale { get; private set; }
+    }
+}

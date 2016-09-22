7c819f0dadfc3026c2c35a3a6932e406623d723b
r15h1 <tapsee.rishi@gmail.com>
2016-09-22 15:16:30 -0400

    #72 - cahce stopwords and show related posts on details page

---
+++ src/Composr.Lib/Util/Cache.cs
@@ -0,0 +1,39 @@
+﻿using Composr.Core;
+using Composr.Repository.Sql;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+
+namespace Composr.Lib.Util
+{
+    public class Cache
+    {
+        private static ConcurrentDictionary<Blog, ISet<string>> stopwords = new ConcurrentDictionary<Blog, ISet<string>>(new BlogComparer());
+        internal static ISet<string> GetStopWords(Blog blog)
+        {
+            if (!stopwords.ContainsKey(blog)) TryFetchStopWordsFromDB(blog);
+
+            return stopwords[blog];
+        }
+
+        private static void TryFetchStopWordsFromDB(Blog blog)
+        {
+            BlogRepository repository = new BlogRepository(new Specifications.MinimalBlogSpecification());
+            ISet<string> sw = repository.GetStopWords(blog);
+            if (sw != null) stopwords.TryAdd(blog, sw);
+        }
+    }
+
+    public class BlogComparer : EqualityComparer<Blog>
+    {
+        public override bool Equals(Blog x, Blog y)
+        {
+            return x.Locale == y.Locale && x.Id == y.Id;                        
+        }
+
+        public override int GetHashCode(Blog obj)
+        {
+            return obj.Locale.GetHashCode() + (string.IsNullOrWhiteSpace(obj.Url) ? 0 : obj.Url.GetHashCode());
+        }
+    }
+}
